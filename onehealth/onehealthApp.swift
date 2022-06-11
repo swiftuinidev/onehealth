52e8f87c81337b7d06f2c7543aa9fa850b3b9006
David Buckley <swiftuinidev@gmail.com>
2022-06-11 15:11:21 +0100

    Initial Commit

---
+++ onehealth/onehealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  onehealthApp.swift
+//  onehealth
+//
+//  Created by David Buckley on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct onehealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
